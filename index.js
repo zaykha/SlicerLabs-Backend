867b28d7b2bade7a38dd2a59d28b2c2187038348
zaykha <zaykha@gmail.com>
2023-08-14 22:30:04 +0630

    debug: cors config 2

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-import express from 'express';
-import  MiddleWareapp  from './src/MiddleWare.js';
-import cors from 'cors';
+import express from "express";
+import MiddleWareapp from "./src/MiddleWare.js";
+import cors from "cors";
 
 const app = express();
 const port = 3000; // Choose the port you want to run your server on
 // Enable CORS
-const allowedOrigins = ['https://main--slicerlabs.netlify.app/', 'https://checkout.stripe.com','http://192.168.1.5:5173/'];
-app.use(cors({
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  // exposedHeaders: ['Authorization'], // Expose the Authorization header
-}));
-
+const allowedOrigins = [
+  "https://main--slicerlabs.netlify.app",
+  "https://checkout.stripe.com",
+  "http://192.168.1.5:5173",
+];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    // exposedHeaders: ['Authorization'], // Expose the Authorization header
+  })
+);
 
 app.use(express.json()); // To parse JSON request bodies
 app.use(MiddleWareapp);
 
-app.listen(process.env.PORT ||port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Server is running on port ${port}`);
 });
